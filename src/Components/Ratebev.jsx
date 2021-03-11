2d5d1af7b71f005bb6f3ed192ecb273db090e323
dorianl <dorian.lopez001@gmail.com>
2021-03-11 11:05:33 -0500

    added other async functions

--- src/Components/Ratebev.jsx
+++ src/Components/Ratebev.jsx
@@ -7,7 +7,9 @@ function Ratebev(props) {
     const [comments, setComments] = useState([]);
   let http = "https://energydrinkrater-be.herokuapp.com";
 
-    const [body, setBody] = useState({body: " ", title: " ", username: " " });
+    const [body, setBody] = useState({body: " ", title: " ", username: " "});
+
+    const [id, setId] = useState({id: ""})
 
     const bodyChange = (event) => {
         setBody({...body, [event.target.className]: event.target.value})
@@ -34,20 +36,63 @@ function Ratebev(props) {
     await fetch("https://energydrinkrater-be.herokuapp.com", {
     method: "POST",
     headers: {
-      "Content-Type": "Application/Type"
+      "Content-Type": "application/json"
     },
-    body: JSON.stringify(body)  
+    body: JSON.stringify({
+      id: id.id,
+      title: body.title,
+      body: body.body
+    })  
     })
     .then ((res) => res.json())
     .then ((res) => {
         setComments(res);
     }) 
     .catch((error) => {
-      console.log(error);
+      // console.log(error);
     })
     console.log(body)
   } 
 
+
+  async function editComment(event) {
+    event.preventDefault();
+    await fetch("https://energydrinkrater-be.herokuapp.com", {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(body)  
+    })
+    .then ((res) => res.json())
+    .then ((res) => {
+        setId(res);
+    }) 
+    .catch((error) => {
+      // console.log(error);
+    })
+    console.log(body)
+  } 
+
+
+  async function deleteComment(event) {
+    event.preventDefault();
+    await fetch("https://energydrinkrater-be.herokuapp.com", {
+    method: "delete",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(body)  
+    })
+    .then ((res) => res.json())
+    .then ((res) => {
+        setId(res);
+    }) 
+    .catch((error) => {
+      // console.log(error);
+    })
+    console.log(body)
+  } 
 
 
     return (
@@ -67,20 +112,20 @@ function Ratebev(props) {
             </form>
           </div>
 
-          <div id = "update-comment">
+          <div id = "edit-comment">
             <h3 className="crud-heading">Edit comment</h3>
-            <form action="https://energydrinkrater-be.herokuapp.com/comment?_method=PUT" method="post" className="postForm">
-                <input type="text" placeholder="id" name="id"></input>
-                <input type="text" placeholder="title" name="title"></input>
-                <input type="text" placeholder="body" name="body"></input>
-                <input type="submit" value="Submit"></input>
+            <form action="https://energydrinkrater-be.herokuapp.com/?_method=PUT" method="post" className="postForm">
+                <input className="id" onChange={bodyChange} type="text" placeholder="id"></input>
+                <input className="title" onChange={bodyChange} type="text" placeholder="title"></input>
+                <textarea className="body" onChange={bodyChange} type="text" placeholder="body"></textarea>
+                <input onClick={editComment} type="submit" value="Submit"></input>
             </form>
           </div>
 
           <div id = "Delete-comment">
             <h3 className="crud-heading">Delete Comment</h3>
             <form action="https://energydrinkrater-be.herokuapp.com/comment?_method=DELETE" method="post" className="postForm">
-                <input type="text" placeholder="ID" name="id"></input>
+                <input className="id" onClick={deleteComment} onChange={bodyChange} type="text" placeholder="ID"></input>
                 <input type="submit" value="Submit"></input>
             </form>
           </div>
